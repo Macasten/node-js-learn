568aefde3362450dd16fb71a5a7e21b598c38589
filipe.goncalves <filipe.goncalves@fabamaq.com>
2020-01-08 17:47:59 +0000

    Importing npm Modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,17 +1,10 @@
-// const utils = require('./utils');
-
-
-// console.log(utils.name);
-// console.log(utils.add(1,3));
-
-//
-// Chanllenge: Define and use a function in a new file
-//
-// 1. Create a new file called notes.js
-// 2. Create getNotes function that returns "Your notes..."
-// 3. Export getNotes function
-// 4. From app.js, load in and call the function printing message to console
-
+const validator = require('validator');
 const getNotes = require('./notes');
 
-console.log( getNotes());
+console.log( getNotes() );
+
+console.log( validator.isEmail('example@gmai.com'))
+console.log( validator.isEmail('examplegmai.com'))
+
+console.log( validator.isURL('http://www.npmjs.com'))
+console.log( validator.isURL('http/www.npmjs.com'))
