db1bfdae52e693be012ae93fefd9019f893aa713
filipe.goncalves <filipe.goncalves@fabamaq.com>
2020-01-08 18:03:08 +0000

    12. Printing in Color

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,15 @@
-const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes');
 
 console.log( getNotes() );
 
-console.log( validator.isEmail('example@gmai.com'))
-console.log( validator.isEmail('examplegmai.com'))
+// Challenge: Use the chalk library in your project
 
-console.log( validator.isURL('http://www.npmjs.com'))
-console.log( validator.isURL('http/www.npmjs.com'))
+// 1. Install version 2.4.1 of chalk
+// 2. Load chalk info applicationCache.js
+// 3. Use it to print the string "Success!" to the  console in green
+// 4. Test your work
+
+// Bonus: use docs to mess around with other styles. Make text bold and inversed.
+
+console.log(chalk.green.bold.inverse("Success!") );
