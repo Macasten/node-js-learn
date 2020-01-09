9f3acf38d8a411abd4d8b5bee44dc713dd9b1a50
filipe.goncalves <filipe.goncalves@fabamaq.com>
2020-01-09 17:33:39 +0000

    15. Getting Input from Users.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,15 +1,11 @@
 const chalk = require('chalk');
 const getNotes = require('./notes');
 
-console.log( getNotes() );
+const command = process.argv[2]
+console.log(process.argv);
 
-// Challenge: Use the chalk library in your project
-
-// 1. Install version 2.4.1 of chalk
-// 2. Load chalk info applicationCache.js
-// 3. Use it to print the string "Success!" to the  console in green
-// 4. Test your work
-
-// Bonus: use docs to mess around with other styles. Make text bold and inversed.
-
-console.log(chalk.green.bold.inverse("Success!") );
+if( command === 'add'){
+  console.log('Adding note!');
+} else if( command === 'remove' ){
+  console.log('Removing note!');
+}
