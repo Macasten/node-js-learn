5ada5526301efbaa6ddbdb1f3a052769e6c9ff39
filipe.goncalves <filipe.goncalves@fabamaq.com>
2020-01-17 18:21:07 +0000

    31. Making HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,8 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout( () => {
-  console.log('2 Second Timer')
-}, 2000)
+const url = 'https://api.darksky.net/forecast/62d2d6aab6e627f7bdd219caae88a070/37.8267,-122.4233'
 
-setTimeout( () => {
-  console.log('0 Second Timer')
-},0)
-
-console.log('Stopping')
+request( { url: url} , ( error, response ) => {
+  const data = JSON.parse( response.body )
+  console.log( data.currently )
+} )
