7e017ee65d15d3656766307bac4a223e1ae6edce
filipe.goncalves <filipe.goncalves@fabamaq.com>
2020-01-20 15:56:05 +0000

    32. Customizing HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,19 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/62d2d6aab6e627f7bdd219caae88a070/37.8267,-122.4233'
 
-request( { url: url} , ( error, response ) => {
-  const data = JSON.parse( response.body )
-  console.log( data.currently )
+
+// Goal: Print a small forecast to the user
+
+// 1. Print: "It is currently 58.55 degrees out. There is a 0% of rain."
+// 2. Test your Work!
+
+//const url = 'https://api.darksky.net/forecast/62d2d6aab6e627f7bdd219caae88a070/37.8267,-122.4233'
+//const url = 'https://api.darksky.net/forecast/62d2d6aab6e627f7bdd219caae88a070/37.8267,-122.4233?key=value1&key2=value2'
+const url = 'https://api.darksky.net/forecast/62d2d6aab6e627f7bdd219caae88a070/37.8267,-122.4233?units=si&lang=pt'
+
+request( { url: url, json: true } , ( error, response ) => {
+  var currently = response.body.currently
+  var daily = response.body.daily
+  console.log(`${ daily.data[0].summary} Is is currently ${currently.temperature} degrees\
+    outerHeight. There is a ${currently.precipProbability}% of rain.`)
 } )
