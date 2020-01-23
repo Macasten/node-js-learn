bdeb8d8ad23b19732581ea537bed433421692454
filipe.goncalves <filipe.goncalves@fabamaq.com>
2020-01-23 17:15:45 +0000

    44. Serving up HTML an JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,25 +2,33 @@ const express = require('express')
 
 const app = express()
 
-// Goal: Setup two new routes
+// Goal: Update routes
 
-// 1. Setup an about page and render a page title
-// 2. Setup a weather route and render a page title
+// 1. Setup about route to render a title with HTML
+// 2. Setup a weather route to send back JSON
+//   - Object with forecast and location strings
 // 3. Test your work by visiting both in the browser
 
 
 app.get('', ( req, res ) =>{
-  res.send('Hello express!')
+  res.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (req, res ) => {
-  res.send('Help page')
+  res.send([{
+    name : "Macas",
+    age : 33
+  },
+  {
+    name : "Mais cenas",
+    age : 0
+  }])
 })
 app.get('/about', (req, res ) => {
-  res.send('About page')
+  res.send('<h1>About page</h1>')
 })
 app.get('/weather', (req, res ) => {
-  res.send('Weather page')
+  res.send({ forecast : "10º", location: "Porto"})
 })
 
 app.listen(3000, () =>{
