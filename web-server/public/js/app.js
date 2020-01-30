eb9a7a4d2d63f2fbb8381b9d1c1fac3815fb1fab
filipe.goncalves <filipe.goncalves@fabamaq.com>
2020-01-30 17:50:46 +0000

    58. Creating a Search Form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,22 +1,28 @@
 console.log('Client side javascript file is loaded!')
 
-fetch( 'http://puzzle.mead.io/puzzle').then( (response) => {
-  
-  response.json().then( (data) => {
-    console.log( data )
-  })
 
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener( 'submit' , (e) => {
+  e.preventDefault()
+  const location = search.value
+  console.log(location)
+  fetch( `http://172.21.31.98:3000/weather?address=${location}` ).then( (response) => {
+    response.json().then( (data) => {
+      console.log( data )
+      if( data.error ){
+        console.log(data.error)
+      } else {
+        console.log( data.location )
+        console.log( data.forecast )
+      }
+    })
+  } )
 })
 
-fetch( 'http://172.21.31.98:3000/weather?address=Porto' ).then( (response) => {
-  console.log(response)
-  response.json().then( (data) => {
-    console.log( data )
-    if( data.error ){
-      console.log(data.error)
-    } else {
-      console.log( data.location )
-      console.log( data.forecast )
-    }
-  })
-} ) 
+// Goal: Use input value to get weather
+
+// 1. Migrate fetch call into the submit callback
+// 2. Use the search text as the address query string value
+// 3. Submit the form with a valid and invalid value to test
