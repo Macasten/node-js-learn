0d601e85196aa11f61a5d97aecf00f70726da622
filipe.goncalves <filipe.goncalves@fabamaq.com>
2020-01-30 17:33:04 +0000

    57. Browser HTTP Request with Fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,22 @@
 console.log('Client side javascript file is loaded!')
+
+fetch( 'http://puzzle.mead.io/puzzle').then( (response) => {
+  
+  response.json().then( (data) => {
+    console.log( data )
+  })
+
+})
+
+fetch( 'http://172.21.31.98:3000/weather?address=Porto' ).then( (response) => {
+  console.log(response)
+  response.json().then( (data) => {
+    console.log( data )
+    if( data.error ){
+      console.log(data.error)
+    } else {
+      console.log( data.location )
+      console.log( data.forecast )
+    }
+  })
+} ) 
